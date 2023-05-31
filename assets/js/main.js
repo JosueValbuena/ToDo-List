90862a32559bcd48455914cc0aa790e9f4e5e202
JosueValbuena <joshvlbn@gmail.com>
2023-05-31 13:42:46 -0400

    avance en el proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 // seleccionando los componentes del input
-const mainTodoListAddInput = document.querySelector(".main-toDoList-add-input");
-const mainTodoListButton = document.querySelector(".main-toDoList-button");
-const mainTodoListAddListUl = document.querySelector(".main-toDoList-add-list");
+const mainTodoListAddInput = document.querySelector("#main-toDoList-add-components-input");
+const mainTodoListButtonAdd = document.querySelector("#main-toDoList-components-buttonAdd");
+const mainTodoListButtonEraseAll = document.querySelector("#main-toDoList-components-buttonEraseAll");
+const mainTodoListAddListUl = document.querySelector("#main-toDoList-add-list");
 
-//
-mainTodoListButton.addEventListener("click", () => agregarTarea());
+// agregando funcionalidad a los botones
+mainTodoListButtonAdd.addEventListener("click", () => agregarTarea());
+mainTodoListButtonEraseAll.addEventListener("click", () => vaciarLista());
 
-const listaTareas = [];
+//array que guarda objetos
+let listaTareas = [];
 
+//vacias la lista de tareas
+function vaciarLista(){
+    listaTareas = [];
+    mainTodoListAddListUl.innerHTML = "";
+}
+
+//constructor de tareas
 class tareasObj {
     constructor(value){
         this.id = Date.now().toLocaleString();
@@ -16,23 +26,24 @@ class tareasObj {
     }
 }
 
+//agregar tareas al objeto desde un input main
 function agregarTarea(){
     const tareaParaAgrear = new tareasObj (mainTodoListAddInput.value);
-    console.log(tareaParaAgrear);
     listaTareas.push(tareaParaAgrear);
-    console.log(listaTareas);
     rendeizarTareas()
 }
 
+//renderizar tareas en el html main input
 function rendeizarTareas(){
     let renderizar = listaTareas.map((tarea) =>
     `
     <li>
     <p>${tarea.descripcion}</p>
-    <button>Completar Tarea</button>
-    <button>Eliminar Tarea</button>
+    <button id=${tarea.id}>Completar Tarea</button>
+    <button id=${tarea.id}>Eliminar Tarea</button>
     </li>
     `
     )
     mainTodoListAddListUl.innerHTML = renderizar;
+    console.log(listaTareas);
 }
